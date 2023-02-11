f339723bcb4a4f2631e148ba47725d7d715eacd5
Seshal Jain <seshaljain11@gmail.com>
2023-02-11 10:56:23 +0530

    Add CodeMirror editor

---
+++ src/routes/code/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import CodeMirror, { basicSetup } from './CodeMirror.svelte';
+
+	let store;
+
+	function changeHandler({ detail: { tr } }) {
+		console.log('change', tr.changes.toJSON());
+	}
+</script>
+
+<div>
+	<CodeMirror
+		doc={'Edit me!\nAnd here is the second line!!'}
+		bind:docStore={store}
+		extensions={basicSetup}
+		on:change={changeHandler}
+	/>
+</div>
+
+<style>
+	:global(.codemirror) {
+		border: 1px solid #ddd;
+	}
+</style>
